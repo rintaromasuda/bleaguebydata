895893bf77451a2d783f7059b4a906b3d6a566ae
Rintaro Masuda <rintaromasuda@live.com>
2018-12-13 13:56:05 +0900

    Started using b.events

--- scrapers/games_scraper.R
+++ scrapers/games_scraper.R
@@ -7,12 +7,18 @@ if (!require(rvest)) {
 }
 
 for (season in c("2016-17")) {
-  for (league in c("B1")) {
-    teams <- subset(b.teams, Season == season & League == league)
-    for (row in seq(1:nrow(teams))) {
-      team.Id <- teams[row, ]$TeamId
-      team.Name <- teams[row, ]$NameShort
-      print(team.Name)
+  for (league in c("B1", "B2")) {
+    events <- GetEvents(league)
+    for (event_row in seq(1:nrow(events))) {
+      event.Id <- events[event_row, ]$EventId
+      event.Name <- events[event_row, ]$ShortName
+      print(event.Name)
+      teams <- subset(b.teams, Season == season & League == league)
+      for (team_row in seq(1:nrow(teams))) {
+        team.Id <- teams[team_row, ]$TeamId
+        team.Name <- teams[team_row, ]$NameShort
+        print(team.Name)
+      }
     }
   }
 }
