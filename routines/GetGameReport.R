298aeb9470de9b0ca1c2bf025d8f33a06ac841cb
Rintaro Masuda <rintaromasuda@live.com>
2019-12-11 23:15:15 +0900

    Completed the design

--- routines/GetGameReport.R
+++ routines/GetGameReport.R
@@ -1,6 +1,8 @@
-target.key <- 4306
-home.team <- "富山"
-away.team <- "千葉"
+target.key <- 4318
+home.teamName <- ""
+away.teamName <- ""
+game.date <- ""
+y.adjust <- 5
 
 Sys.setlocale(locale = 'Japanese')
 
@@ -23,7 +25,6 @@ remDr <- RSelenium::remoteDriver(remoteServerAddr = "localhost",
                                  port = 4444L,
                                  browserName = "chrome")
 remDr$open()
-
 
 scrapePlayByPlayData <- function(){
   df_pbyp <- data.frame()
@@ -58,6 +59,31 @@ scrapePlayByPlayData <- function(){
   }
 
   # Actual parse
+  home <- html.pbyp %>%
+    html_nodes("#game__top__inner > div.result_wrap > div.team_wrap.home.win > div.team_name > p.for-sp") %>%
+    html_text()
+  if (identical(home, character(0))) {
+    home <- html.pbyp %>%
+      html_nodes("#game__top__inner > div.result_wrap > div.team_wrap.home > div.team_name > p.for-sp") %>%
+      html_text()
+  }
+  away <- html.pbyp %>%
+    html_nodes("#game__top__inner > div.result_wrap > div.team_wrap.away.win > div.team_name > p.for-sp") %>%
+    html_text()
+  if (identical(away, character(0))) {
+    away <- html.pbyp %>%
+      html_nodes("#game__top__inner > div.result_wrap > div.team_wrap.away > div.team_name > p.for-sp") %>%
+      html_text()
+  }
+  
+  home.teamName <<- home
+  away.teamName <<- away
+  
+  date <- html.pbyp %>%
+    html_nodes("#game__top__inner > div.date_wrap > p:nth-child(2) > span") %>%
+    html_text()
+  game.date <<- bleaguer::GetFullDateString(date, b.current.season)
+  
   for (period_node in nodes.period) {
     period_num <- as.integer(html_attr(period_node, "data-period"))
     
@@ -100,108 +126,125 @@ scrapePlayByPlayData <- function(){
   return(df_pbyp)
 }
 
+processPlayByPlayData <- function(df){
+  df$ImageUrl <- as.character(df$ImageUrl)
+  df$PlayerId <- as.integer(sapply(strsplit(sapply(strsplit(df$ImageUrl, "/"), function(x) {x[7]}), "_"), function(x) {x[1]}))
+  df$RemainingMinPeriod <- bleaguer::ConvertMinStrToDec(df$Clock)
+  df$PastMinInPeriod <- ifelse(df$Period <= 4,
+                                    (10 - df$RemainingMinPeriod),
+                                    (5 - df$RemainingMinPeriod))
+  df$PastMinInGame <- ifelse(df$Period <= 4,
+                                  (((df$Period - 1) * 10) + df$PastMinInPeriod),
+                                  (40 + ((df$Period - 5) * 5) + df$PastMinInPeriod))
+  df$PastMinInGameClass <- ceiling(df$PastMinInGame)
+  
+  df %<>%
+    arrange(Period, DataNo) %>%
+    mutate(PtsAdded = ifelse(ActionCd == "1", 3,
+                             ifelse(ActionCd == "3" | ActionCd == "4", 2,
+                                    ifelse(ActionCd == "7", 1, 0)))) %>%
+    mutate(HomePtsAdded = ifelse(grepl("home", HomeAway, ignore.case = TRUE), PtsAdded, 0),
+           AwayPtsAdded = ifelse(grepl("away", HomeAway, ignore.case = TRUE), PtsAdded, 0))
+  
+  df %<>%
+    arrange(Period, DataNo) %>%
+    mutate(HomeAccPts = cumsum(HomePtsAdded),
+           AwayAccPts = cumsum(AwayPtsAdded))
+  
+  df$PtsDiff = df$HomeAccPts - df$AwayAccPts
+  
+  df$Team <- ifelse(grepl("home", df$HomeAway, ignore.case = TRUE), home.teamName, away.teamName)
+  return(df)
+}
 
+df <- scrapePlayByPlayData()
+df <- processPlayByPlayData(df)
 
+############
+# Q labels #
+############
+df_label <-
+  rbind(
+    data.frame(Label = "Q1",  X = 1),
+    data.frame(Label = "Q2",  X = 11),
+    data.frame(Label = "Q3",  X = 21),
+    data.frame(Label = "Q4",  X = 31),
+    data.frame(Label = "OT1", X = 41),
+    data.frame(Label = "OT2", X = 46),
+    data.frame(Label = "OT3", X = 51),
+    data.frame(Label = "OT4", X = 56)
+  )
 
-
-
-
-df_pbyp$ImageUrl <- as.character(df_pbyp$ImageUrl)
-df_pbyp$PlayerId <- as.integer(sapply(strsplit(sapply(strsplit(df_pbyp$ImageUrl, "/"), function(x) {x[7]}), "_"), function(x) {x[1]}))
-df_pbyp$RemainingMinPeriod <- bleaguer::ConvertMinStrToDec(df_pbyp$Clock)
-df_pbyp$PastMinInPeriod <- ifelse(df_pbyp$Period <= 4,
-                                  (10 - df_pbyp$RemainingMinPeriod),
-                                  (5 - df_pbyp$RemainingMinPeriod))
-df_pbyp$PastMinInGame <- ifelse(df_pbyp$Period <= 4,
-                                (((df_pbyp$Period - 1) * 10) + df_pbyp$PastMinInPeriod),
-                                (40 + ((df_pbyp$Period - 5) * 5) + df_pbyp$PastMinInPeriod))
-df_pbyp$PastMinInGameClass <- ceiling(df_pbyp$PastMinInGame)
-
-
-df_pbyp %<>%
-  arrange(Period, DataNo) %>%
-  mutate(PtsAdded = ifelse(ActionCd == "1", 3,
-                           ifelse(ActionCd == "3" | ActionCd == "4", 2,
-                                  ifelse(ActionCd == "7", 1, 0)))) %>%
-  mutate(HomePtsAdded = ifelse(grepl("home", HomeAway, ignore.case = TRUE), PtsAdded, 0),
-         AwayPtsAdded = ifelse(grepl("away", HomeAway, ignore.case = TRUE), PtsAdded, 0))
-
-df_pbyp %<>%
-  arrange(Period, DataNo) %>%
-  mutate(HomeAccPts = cumsum(HomePtsAdded),
-         AwayAccPts = cumsum(AwayPtsAdded))
-
-df_pbyp$PtsDiff = df_pbyp$HomeAccPts - df_pbyp$AwayAccPts
-
-#####
+###########
+# Timeout #
+###########
 df_timeout <-
-  df_pbyp %>%
+  df %>%
   filter(!is.na(PastMinInGameClass)) %>%
   filter(ActionCd == "88") %>%
   as.data.frame()
 
-df_timeout$Team <- ifelse(grepl("home", df_timeout$HomeAway, ignore.case = TRUE), home.team, away.team)
-
-dd <-
-  df_pbyp %>%
+#########
+# Point #
+#########
+df_point_per_min <-
+  df %>%
   filter(!is.na(PastMinInGameClass)) %>%
   arrange(DataNo) %>%
   group_by(PastMinInGameClass) %>%
   summarize(PtsHome = last(HomeAccPts),
             PtsAway = last(AwayAccPts))
 
-dd_text <-
+df_point <-
   rbind(
     data.frame(
-      Label = "Q1",
-      X = 1
+      Team = home.teamName,
+      Min = df_point_per_min$PastMinInGameClass,
+      Pts = df_point_per_min$PtsHome,
+      Diff = df_point_per_min$PtsHome - df_point_per_min$PtsAway
     ),
     data.frame(
-      Label = "Q2",
-      X = 11
-    ),
-    data.frame(
-      Label = "Q3",
-      X = 21
-    ),
-    data.frame(
-      Label = "Q4",
-      X = 31
-    ),
-    data.frame(
-      Label = "OT1",
-      X = 41
-    ),
-    data.frame(
-      Label = "OT2",
-      X = 46
-    ),
-    data.frame(
-      Label = "OT3",
-      X = 51
-    ),
-    data.frame(
-      Label = "OT4",
-      X = 56
+      Team = away.teamName,
+      Min = df_point_per_min$PastMinInGameClass,
+      Pts = df_point_per_min$PtsAway,
+      Diff = df_point_per_min$PtsAway - df_point_per_min$PtsHome
     )
   )
+df_point$Direction = ifelse(df_point$Diff > 0, 1,
+                            ifelse(df_point$Diff < 0, -1,
+                                   ifelse(df_point$Team == home.teamName, 1, -1)))
 
-dd2 <-
-  rbind(
-    data.frame(
-      Team = home.team,
-      Min = dd$PastMinInGameClass,
-      Pts = dd$PtsHome,
-      Diff = dd$PtsHome - dd$PtsAway
-    ),
-    data.frame(
-      Team = away.team,
-      Min = dd$PastMinInGameClass,
-      Pts = dd$PtsAway,
-      Diff = dd$PtsAway - dd$PtsHome
-    )
-  )
-dd2$Direction = ifelse(dd2$Diff > 0, 1, -1)
+#########
+# Stats #
+#########
+df_stats <-
+  df %>%
+  filter(!is.na(PastMinInGameClass)) %>%
+  group_by(Team, Period) %>%
+  summarize(FGM = sum(ActionCd %in% c("1", "3", "4")),
+            FGA = sum(ActionCd %in% c("1", "2", "3", "4", "5", "6")),
+            FTM = sum(ActionCd %in% c("7")),
+            FTA = sum(ActionCd %in% c("7", "8"))) %>%
+  as.data.frame()
+df_stats$X <- ifelse(df_stats$Period <= 5,
+                     (df_stats$Period - 1) * 10 + 5,
+                     (df_stats$Period - 2) * 10 + (df_stats$Period - 5) * 5 + 2.5)
+df_stats$Y <- ifelse(df_stats$Period <= 2, max(df_point$Pts) + y.adjust - 10, 15)
+df_stats$Y <- ifelse(df_stats$Team != home.teamName, df_stats$Y - 7, df_stats$Y)
+
+###############
+# Actual Plot #
+###############
+home.longName <- subset(b.teams, Season == b.current.season & NameShort == home.teamName)[c("NameLong")][[1]]
+away.longName <- subset(b.teams, Season == b.current.season & NameShort == away.teamName)[c("NameLong")][[1]]
+plot.title <- paste0(b.current.season,
+                     "シーズン ",
+                     home.longName,
+                     " vs ",
+                     away.longName,
+                     " (",
+                     game.date,
+                     ")")
 
 ggplot() +
   geom_vline(xintercept =  0, linetype="dashed", color = "grey", size=1) +
@@ -209,27 +252,39 @@ ggplot() +
   geom_vline(xintercept = 20, linetype="dashed", color = "grey", size=1) +
   geom_vline(xintercept = 30, linetype="dashed", color = "grey", size=1) +
   geom_vline(xintercept = 40, linetype="dashed", color = "grey", size=1) +
-  geom_text(data = subset(dd_text, X < max(dd2$Min)),
+  geom_text(data = subset(df_label, X < max(df_point$Min)),
             aes(x = X,
-                y = max(dd2$Pts),
+                y = max(df_point$Pts) + y.adjust,
                 label = as.character(Label)),
             color = "grey") +
-  geom_line(data = dd2,
+  geom_text(data = subset(df_stats, X < max(df_point$Min)),
+            aes(x = X,
+                y = Y,
+                label = paste0("FG: ",
+                               as.character(FGM),
+                               "/",
+                               as.character(FGA),
+                               "\n",
+                               "FT: ",
+                               as.character(FTM),
+                               "/",
+                               as.character(FTA)),
+                color = Team)) +  
+  geom_line(data = df_point,
             aes(x = Min,
                 y = Pts,
                 color = Team),
             size = 1) +
-  geom_point(data = dd2,
+  geom_point(data = df_point,
             aes(x = Min,
                 y = Pts,
                 color = Team),
             shape = 15,
             size = 3) +
-  geom_label(data = subset(dd2, Min %in% c(10, 20, 30, 40, 45, 50, 55, 60, 65)),
+  geom_label(data = subset(df_point, Min %in% c(10, 20, 30, 40, 45, 50, 55, 60, 65)),
                    show.legend = FALSE,
                    aes(x = Min,
-                       y = Pts + ifelse(Diff != 0, Direction * 5, 
-                                        ifelse(Team == home.team, 5, -5)),
+                       y = Pts + (Direction * 5), 
                        label = as.character(Pts),
                        color = Team),
                    size = 5) +
@@ -241,14 +296,21 @@ ggplot() +
              size = 3) +
   xlab("経過時間") +
   ylab("得点") +
-  labs(title = "富山グラウジーズ vs 千葉ジェッツ (12/08)",
+  labs(title = plot.title,
        subtitle = "▲はチームが取得したタイムアウト") +
   theme_bw() +
   theme(
     legend.title = element_blank(),
-    legend.text = element_text(size = 15)
+    legend.text = element_text(size = 12)
   )
 
-ggsave("PtsHistory_4306.jpg", width = 9, height = 6)
+###############
+# Output File #
+###############
+fileName <- paste0("PtsHistory_",
+                   target.key,
+                   ".jpg")
+ggsave(fileName, width = 9, height = 6)
+
 
 
