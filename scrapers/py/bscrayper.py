2fa994a203a0b37fd233a999c2d800d851b2ca2f
Rintaro Masuda <rintaromasuda@live.com>
2020-02-20 00:25:10 +0900

    Now got the JSON

--- scrapers/py/bscrayper.py
+++ scrapers/py/bscrayper.py
@@ -5,12 +5,19 @@ def main():
     res = rq.get("https://www.bleague.jp/schedule/?tab=1&year=2019&event=2&club=&setuFrom=1&setuTo=36")
     soup = BeautifulSoup(res.content, 'html.parser')
 
-    for div in soup.find_all('div', {'class': "state_link btn report"}):
-        detail_link = div.a['href']
+    for div_tag in soup.find_all('div', {'class': "state_link btn report"}):
+        detail_link = div_tag.a['href']
         res = rq.get(detail_link)
         soup = BeautifulSoup(res.content, 'html.parser')
         print(soup.title)
-        print("\n")
+
+        for script_tag in soup.find_all('script'):
+            start_index = script_tag.text.find("_contexts_s3id.data")
+            if (start_index > -1):
+                content = script_tag.text[start_index:]
+                start_index = content.find("{\"")
+                end_index = content.find("};")
+                json_str = content[start_index:end_index]
         break
 
 if __name__ == "__main__":
