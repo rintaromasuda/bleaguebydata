a1d588934638b239e3435059d89fc0c50baa3d9b
Rintaro Masuda <rintaromasuda@live.com>
2021-02-16 09:02:23 +0900

    More scripts

---
+++ routines/CurrentOffensiveNumbers.R
@@ -0,0 +1,158 @@
+library(bleaguer)
+library(dplyr)
+library(ggplot2)
+
+targetSeason <- b.current.season
+
+df <- subset(GetGameSummary(),
+             Season == targetSeason &
+             Category == "Regular")
+df.teams <- subset(b.teams, Season == b.current.season)[c("TeamId", "NameShort")]
+df <- merge(df, df.teams, by = "TeamId")
+df$TeamName <- df$NameShort
+df$NameShort <- NULL
+
+###
+# PtsSecondChance
+###
+
+plot2ndPts <- function(df, league = "B1"){
+  df <- subset(df, League == league)
+  df %<>%
+    group_by(TeamId, TeamName) %>%
+    mutate(Med = median(PtsSecondChance),
+           Avg = mean(PtsSecondChance),
+           TeamNameN = paste0(TeamName, " (", n(), ")"))
+
+  ggplot() +
+    geom_boxplot(data = df,
+                 aes(x = reorder(TeamNameN, Med),
+                     y = PtsSecondChance)) +
+    geom_point(data = df,
+               aes(x = reorder(TeamNameN, Med),
+                   y = Avg),
+               shape = 4,
+               color = "blue") +
+    labs(title = paste(targetSeason, "レギュラシーズン", "セカンドチャンス得点"),
+         subtitle = "中央値順での並び。xは1試合平均値。()内はデータ内の経過試合数。",
+         x = "",
+         y = "") +
+    theme_gray() +
+    theme(
+      axis.text.y = element_text(size = 12)
+    ) +
+    coord_flip()
+}
+plot2ndPts(df)
+ggsave("2ndPts_B1.jpg", width = 6, height = 9)
+plot2ndPts(df, "B2")
+ggsave("2ndPts_B2.jpg", width = 6, height = 9)
+
+###
+# PtsInPaint
+###
+
+plotPaintPts <- function(df, league = "B1"){
+  df <- subset(df, League == league)
+  df %<>%
+    group_by(TeamId, TeamName) %>%
+    mutate(Med = median(PtsInPaint),
+           Avg = mean(PtsInPaint),
+           TeamNameN = paste0(TeamName, " (", n(), ")"))
+
+  ggplot() +
+    geom_boxplot(data = df,
+                 aes(x = reorder(TeamNameN, Med),
+                     y = PtsInPaint)) +
+    geom_point(data = df,
+               aes(x = reorder(TeamNameN, Med),
+                   y = Avg),
+               shape = 4,
+               color = "blue") +
+    labs(title = paste(targetSeason, "レギュラシーズン", "ペイント内得点"),
+         subtitle = "中央値順での並び。xは1試合平均値。()内はデータ内の経過試合数。",
+         x = "",
+         y = "") +
+    theme_gray() +
+    theme(
+      axis.text.y = element_text(size = 12)
+    ) +
+    coord_flip()
+}
+plotPaintPts(df)
+ggsave("PaintPts_B1.jpg", width = 6, height = 9)
+plotPaintPts(df, "B2")
+ggsave("PaintPts_B2.jpg", width = 6, height = 9)
+
+###
+# .EFG
+###
+plotEFG <- function(df, league = "B1"){
+  df <- subset(df, League == league)
+  df %<>%
+    group_by(TeamId, TeamName) %>%
+    mutate(Med = median(EFG),
+           Avg = ((sum(F3GM) * 1.5) + sum(F2GM)) / (sum(F3GA) + sum(F2GA)),
+           TeamNameN = paste0(TeamName, " (", n(), ")"))
+
+  ggplot() +
+    geom_boxplot(data = df,
+                 aes(x = reorder(TeamNameN, Med),
+                     y = EFG)) +
+    geom_point(data = df,
+               aes(x = reorder(TeamNameN, Med),
+                   y = Avg),
+               shape = 4,
+               color = "blue") +
+    scale_y_continuous(labels = scales::percent) +
+    labs(title = paste(targetSeason, "レギュラシーズン", "eFG%"),
+         subtitle = "中央値順での並び。xは累計ベースの値。()内はデータ内の経過試合数。",
+         x = "",
+         y = "") +
+    theme_gray() +
+    theme(
+      axis.text.y = element_text(size = 12)
+    ) +
+    coord_flip()
+}
+plotEFG(df)
+ggsave("EFG_B1.jpg", width = 6, height = 9)
+plotEFG(df, "B2")
+ggsave("EFG_B2.jpg", width = 6, height = 9)
+
+###
+# TO
+###
+
+plotTO <- function(df, league = "B1"){
+  df <- subset(df, League == league)
+  df %<>%
+    group_by(TeamId, TeamName) %>%
+    mutate(Med = median(TO),
+           Avg = mean(TO),
+           TeamNameN = paste0(TeamName, " (", n(), ")"))
+
+  ggplot() +
+    geom_boxplot(data = df,
+                 aes(x = reorder(TeamNameN, desc(Med)),
+                     y = TO)) +
+    geom_point(data = df,
+               aes(x = reorder(TeamNameN, desc(Med)),
+                   y = Avg),
+               shape = 4,
+               color = "blue") +
+    labs(title = paste(targetSeason, "レギュラシーズン", "ターンオーバー"),
+         subtitle = "中央値順での並び。xは1試合平均値。()内はデータ内の経過試合数。",
+         x = "",
+         y = "") +
+    theme_gray() +
+    theme(
+      axis.text.y = element_text(size = 12)
+    ) +
+    coord_flip()
+}
+plotTO(df)
+ggsave("TO_B1.jpg", width = 6, height = 9)
+plotTO(df, "B2")
+ggsave("TO_B2.jpg", width = 6, height = 9)
+
--- routines/GetShooterCharts.R
+++ routines/GetShooterCharts.R
@@ -4,124 +4,118 @@ library(ggplot2)
 library(ggrepel)
 
 targetSeason <- b.current.season
+targetLeague <- "B1"
 
-plotCharts <- function(targetLeague) {
-  df <- subset(GetGameSummary(),
-               Season == targetSeason &
-                 Category == "Regular" &
-                 League == targetLeague)
-  
-  df.boxscore <- subset(b.games.boxscore, ScheduleKey %in% df$ScheduleKey)
-  df.boxscore %<>% # In case a player has two different names
-    group_by(PlayerId) %>%
-    mutate(Player = last(Player),
-           Counter = 1)
-  
-  df.box.agg <-
-    df.boxscore %>%
-    group_by(PlayerId, Player) %>%
-    summarise(GP = sum(Counter[MIN > 0]),
-              PTS = sum(PTS),
-              FGA = sum(FGA),
-              FGM = sum(FGM),
-              F3GA = sum(F3GA),
-              F3GM = sum(F3GM),
-              FTA = sum(FTA),
-              FTM = sum(FTM)) %>%
-    mutate(PPG = (PTS / GP),
-           FGR = (FGM / FGA),
-           F3GR = (F3GM / F3GA),
-           FTR = (FTM / FTA))
-  
-  df.box.agg$PPG_SD <- scale(df.box.agg$PPG)
-  df.box.agg$F3GA_SD <- scale(df.box.agg$F3GA)
-  df.box.agg$F3GM_SD <- scale(df.box.agg$F3GM)
-  df.box.agg$FTA_SD <- scale(df.box.agg$FTA)
-  df.box.agg$FTM_SD <- scale(df.box.agg$FTM)
-  
-  FGR_Avg <- sum(df.boxscore$FGM) / sum(df.boxscore$FGA)
-  F3GR_Avg <- sum(df.boxscore$F3GM) / sum(df.boxscore$F3GA)
-  FTR_Avg <- sum(df.boxscore$FTM) / sum(df.boxscore$FTA)
-  
-  df.box.agg$F3GR_IS_GTE_AVG <- (df.box.agg$F3GR >= F3GR_Avg)
-  df.box.agg$F3GR_NUDGE <- ifelse(df.box.agg$F3GR_IS_GTE_AVG, 0.4, -0.2)
-  df.box.agg$F3GR_IS_TARGET <- (df.box.agg$F3GA_SD >= 1.7 |
-                                  df.box.agg$F3GM_SD >= 1.7 |
-                                  df.box.agg$PlayerId %in% c())
-  df.box.agg$FTR_IS_GTE_AVG <- (df.box.agg$FTR >= FTR_Avg)
-  df.box.agg$FTR_NUDGE <- ifelse(df.box.agg$FTR_IS_GTE_AVG, 0.4, -0.2)
-  df.box.agg$FTR_IS_TARGET <- (df.box.agg$FTA_SD >= 1.7 |
-                                 df.box.agg$FTM_SD >= 1.7 |
-                                 (df.box.agg$FTA >= 50 & df.box.agg$FTR >= 0.9) |
-                                 (df.box.agg$FTA >= 50 & df.box.agg$FTR < 0.5) |
-                                 df.box.agg$PlayerId %in% c())  
+df <- subset(GetGameSummary(),
+             Season == targetSeason &
+             Category == "Regular" &
+             League == targetLeague)
+dim(df)
 
-  # Visualize
+df.boxscore <- subset(b.games.boxscore, ScheduleKey %in% df$ScheduleKey)
+df.boxscore %<>% # In case a player has two different names
+  group_by(PlayerId) %>%
+  mutate(Player = last(Player),
+         Counter = 1)
 
-  ggplot() +
-    geom_point(data = df.box.agg,
-               aes(x = F3GA,
-                   y = F3GR,
-                   size = PPG),
-               alpha = 0.5) +
-    geom_hline(yintercept = F3GR_Avg, linetype = 2, size = 1, color = "purple") +
-    geom_text_repel(data = subset(df.box.agg, F3GR_IS_TARGET),
-                    aes(x = F3GA,
-                        y = F3GR,
-                        label = Player),
-                    nudge_y = subset(df.box.agg, F3GR_IS_TARGET)$F3GR_NUDGE) +
-    scale_y_continuous(labels = scales::percent) +
-    labs(title = paste(targetSeason, "レギュラシーズン", "3ポイントシュート (", targetLeague, ")"),
-         subtitle = "点線はリーグ平均%、点の大きさはその選手の1試合平均得点を表示。",
-         x = "試投数",
-         y = "成功率") +
-    theme_gray() +
-    theme_bw() +
-    theme(
-      plot.title = element_text(hjust = 0.5),
-      plot.subtitle = element_text(hjust = 0.5),
-      axis.text.y = element_text(size = 12),
-      axis.text.x = element_text(size = 12),
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(),
-      legend.position = "none"
-    )
-  
-  fileName <- paste0("F3G_", targetLeague, ".jpg")
-  ggsave(fileName, width = 9, height = 6)  
-  
-  ggplot() +
-    geom_point(data = df.box.agg,
-               aes(x = FTA,
-                   y = FTR,
-                   size = PPG),
-               alpha = 0.5) +
-    geom_hline(yintercept = FTR_Avg, linetype = 2, size = 1, color = "purple") +
-    geom_text_repel(data = subset(df.box.agg, FTR_IS_TARGET),
-                    aes(x = FTA,
-                        y = FTR,
-                        label = Player),
-                    nudge_y = subset(df.box.agg, FTR_IS_TARGET)$FTR_NUDGE) +
-    scale_y_continuous(labels = scales::percent, limits = c(0, 1.3)) +
-    labs(title = paste(targetSeason, "レギュラシーズン", "フリースロー (", targetLeague, ")"),
-         subtitle = "点線はリーグ平均%、点の大きさはその選手の1試合平均得点を表示。",
-         x = "試投数",
-         y = "成功率") +
-    theme_gray() +
-    theme_bw() +
-    theme(
-      plot.title = element_text(hjust = 0.5),
-      plot.subtitle = element_text(hjust = 0.5),
-      axis.text.y = element_text(size = 12),
-      axis.text.x = element_text(size = 12),
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(),
-      legend.position = "none"
-    )
-  
-  fileName <- paste0("FT_", targetLeague, ".jpg")
-  ggsave(fileName, width = 9, height = 6)
-}
+df.box.agg <-
+  df.boxscore %>%
+  group_by(PlayerId, Player) %>%
+  summarise(GP = sum(Counter[MIN > 0]),
+            PTS = sum(PTS),
+            FGA = sum(FGA),
+            FGM = sum(FGM),
+            F3GA = sum(F3GA),
+            F3GM = sum(F3GM),
+            FTA = sum(FTA),
+            FTM = sum(FTM)) %>%
+  mutate(PPG = (PTS / GP),
+         FGR = (FGM / FGA),
+         F3GR = (F3GM / F3GA),
+         FTR = (FTM / FTA))
 
-plotCharts("B1")
-plotCharts("B2")
+df.box.agg$PPG_SD <- scale(df.box.agg$PPG)
+df.box.agg$F3GA_SD <- scale(df.box.agg$F3GA)
+df.box.agg$F3GM_SD <- scale(df.box.agg$F3GM)
+df.box.agg$FTA_SD <- scale(df.box.agg$FTA)
+df.box.agg$FTM_SD <- scale(df.box.agg$FTM)
+
+FGR_Avg <- sum(df.boxscore$FGM) / sum(df.boxscore$FGA)
+F3GR_Avg <- sum(df.boxscore$F3GM) / sum(df.boxscore$F3GA)
+FTR_Avg <- sum(df.boxscore$FTM) / sum(df.boxscore$FTA)
+
+df.box.agg$F3GR_IS_GTE_AVG <- (df.box.agg$F3GR >= F3GR_Avg)
+df.box.agg$F3GR_NUDGE <- ifelse(df.box.agg$F3GR_IS_GTE_AVG, 0.4, -0.2)
+df.box.agg$F3GR_IS_TARGET <- (df.box.agg$F3GA_SD >= 1.7 |
+                              df.box.agg$F3GM_SD >= 1.7 |
+                              df.box.agg$PlayerId %in% c(8456,
+                                                         10815,
+                                                         33093,
+                                                         33031,
+                                                         32994))
+df.box.agg$FTR_IS_GTE_AVG <- (df.box.agg$FTR >= FTR_Avg)
+df.box.agg$FTR_NUDGE <- ifelse(df.box.agg$FTR_IS_GTE_AVG, 0.4, -0.2)
+df.box.agg$FTR_IS_TARGET <- (df.box.agg$FTA_SD >= 1.7 |
+                                df.box.agg$FTM_SD >= 1.7 |
+                                (df.box.agg$FTA >= 75 & df.box.agg$FTR >= 0.8))
+
+# Visualize
+
+ggplot() +
+  geom_point(data = df.box.agg,
+             aes(x = F3GA,
+                 y = F3GR,
+                 size = PPG),
+             alpha = 0.5) +
+  geom_hline(yintercept = F3GR_Avg, linetype = 2, size = 1, color = "purple") +
+  geom_text_repel(data = subset(df.box.agg, F3GR_IS_TARGET),
+                   aes(x = F3GA,
+                       y = F3GR,
+                       label = Player),
+                  nudge_y = subset(df.box.agg, F3GR_IS_TARGET)$F3GR_NUDGE) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(title = paste(targetSeason, "レギュラシーズン", "3ポイントシュート (", targetLeague, ")"),
+       subtitle = "点線はリーグ平均%、点の大きさはその選手の1試合平均得点を表示。",
+       x = "試投数",
+       y = "成功率") +
+  theme_gray() +
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5),
+    axis.text.y = element_text(size = 12),
+    axis.text.x = element_text(size = 12),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    legend.position = "none"
+  )
+
+ggplot() +
+  geom_point(data = df.box.agg,
+             aes(x = FTA,
+                 y = FTR,
+                 size = PPG),
+             alpha = 0.5) +
+  geom_hline(yintercept = FTR_Avg, linetype = 2, size = 1, color = "purple") +
+  geom_text_repel(data = subset(df.box.agg, FTR_IS_TARGET),
+                  aes(x = FTA,
+                      y = FTR,
+                      label = Player),
+                  nudge_y = subset(df.box.agg, FTR_IS_TARGET)$FTR_NUDGE) +
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1.4)) +
+  labs(title = paste(targetSeason, "レギュラシーズン", "フリースロー (", targetLeague, ")"),
+       subtitle = "点線はリーグ平均%、点の大きさはその選手の1試合平均得点を表示。",
+       x = "試投数",
+       y = "成功率") +
+  theme_gray() +
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5),
+    axis.text.y = element_text(size = 12),
+    axis.text.x = element_text(size = 12),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    legend.position = "none"
+  )
+ggsave("FT_B1.jpg", width = 9, height = 6)
